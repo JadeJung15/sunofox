8b0b5bfe1a5217faadd164add515ef7f938127a2
JadeJung15 <jadejung15@gmail.com>
2026-06-07 16:56:40 +0900

    style: reduce SunoFox home icon size

--- sf-studio-sw.js
+++ sf-studio-sw.js
@@ -1,8 +1,8 @@
-const SF_STUDIO_CACHE = 'sf-studio-20260607-linkhub';
+const SF_STUDIO_CACHE = 'sf-studio-20260607-icon';
 const SF_STUDIO_ASSETS = [
   '/manifest.webmanifest',
   '/css/style.css?v=20260607-fanpage',
-  '/css/sf-home.css?v=20260607-linkhub',
+  '/css/sf-home.css?v=20260607-icon',
   '/css/sf-category.css?v=20260607-categories',
   '/css/sf-community.css?v=20260607-d1-community',
   '/css/mv-storyboard.css?v=20260607-auth',
